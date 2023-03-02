5a3788c1edd2396aa4399c244dd445dd68bf8526
Alex Granovsky <compfaculty@users.noreply.github.com>
2023-03-02 22:41:31 +0200

    ver1

---
+++ HttpScaner/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using HttpScaner;
+
+var filename = args[0];
+string?[] urls = File.ReadAllLines(filename);
+Console.WriteLine($"reading urls from {filename}");
+HttpClientHandler clientHandler = new HttpClientHandler();
+clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+var tasks = new List<Task<TargetResponse>>();
+
+foreach (string? url in urls)
+{
+    tasks.Add(Task.Run(async () =>
+    {
+        using (HttpClient client = new HttpClient(clientHandler))
+        {
+            var res = new TargetResponse() { Target = url, Status = false };
+            try
+            {
+                if (client != null)
+                {
+                    var resp = await client.GetAsync(url);
+                    if (resp.IsSuccessStatusCode)
+                    {
+                        res.Status = true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // ignored
+                // Console.WriteLine($"{e}");
+            }
+
+            return res;
+        }
+    }));
+}
+
+
+var responses = await Task.WhenAll(tasks.ToArray());
+
+
+foreach (var r in responses)
+{
+    Console.WriteLine($"{r.Target} {r.Status}");
+}
---
+++ HttpScaner/TargetResponse.cs
@@ -0,0 +1,7 @@
+namespace HttpScaner;
+
+public class TargetResponse
+{
+    public string? Target { get; set; }
+    public bool Status { get; set; }
+}
